927f4c5777642011ac2c7abe2a80ee220de9dec9
nickKearns <nickkearns133@gmail.com>
2020-05-15 23:11:30 -0400

    fixed signup

--- petsite/urls.py
+++ petsite/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pets.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-
-
+    path('accounts/', include('accounts.urls'))
 ]
